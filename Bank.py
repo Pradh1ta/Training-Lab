d7d064e5cb90eeb75c1af7cf14e69a32e5cac27a
Pradh1ta <pradhita1010@gmail.com>
2026-06-06 19:49:34 +0700

    Update menambahkan tampilan pertama

--- Bank.py
+++ Bank.py
@@ -1,1 +1,25 @@
-print("Hello World")
+# Database 
+data_user = {
+    "username1": "admin",
+    "password1": "admin123",
+    "pin1": 102007,
+    "usersaldo1": 1500000,
+    "norek1": 1118212855121
+}
+
+loop = True
+while loop:
+    print("╔══════════════════════════════════════╗")
+    print("║        🏦 BANK DIGITAL CLI  🏦       ║")
+    print("║     Sistem Perbankan Versi CLI       ║")
+    print("╚══════════════════════════════════════╝")
+    print()
+    print("Selamat Datang, Nasabah!")
+    print("[1] Login")
+    print("[2] Exit")
+    print()
+    inputfirst = input("> Masukan Pilihan : ")
+    if inputfirst == "1":
+        print("")
+    elif inputfirst == "2":
+        break
